2df33c13f1a935f5bec05c494fa40ac6af12dd83
Kevin Lin <linkevin@cmu.edu>
2020-12-11 05:40:55 +0100

    Delete more cells for each state

--- src/visualizer/Visualizer.jsx
+++ src/visualizer/Visualizer.jsx
@@ -6,98 +6,154 @@ import './Visualizer.css';
 export default class Visualizer extends Component {
   constructor(props) {
     super(props);
-    this.model = [];
+    this.grid = [];
     this.numRows = 9;
     this.numCols = 15;
     this.state = {
       nodes: [], // (row, col, label, size)
-      edges: []  // (start, stop, probability)
+      edges: new Map()  // "row1,col1" : ["row2,col2,prob", ...]
     };
   }
 
-  generateModel() {
-    let newModel = [];
+  // generate default grid
+  generateGrid() {
+    let newgrid = [];
     for (let row = 0; row < this.numRows; row++) {
       let newRow = [];
       for (let col = 0; col < this.numCols; col++) {
         newRow.push("");
       }
-      newModel.push(newRow);
+      newgrid.push(newRow);
     }
-    this.model = newModel;
+    this.grid = newgrid;
   }
 
-  grayNeighbors = (row, col) => {
+  // add edge between two states 
+  addEdge() {
+    const newRow1 = parseInt(prompt("Row 1: ")); // improve
+    const newCol1 = parseInt(prompt("Col 1: ")); // improve
+    const label1 = this.grid[newRow1][newCol1];
+    if (label1 === null || label1.length === 0) {
+      alert("Cannot add edge to non-existent state");
+      return;
+    }
+
+    const newRow2 = parseInt(prompt("Row 2: ")); // improve
+    const newCol2 = parseInt(prompt("Col 2: ")); // improve
+    const label2 = this.grid[newRow2][newCol2];
+    if (label2 === null || label2.length === 0) {
+      alert("Cannot add edge to non-existent state");
+      return;
+    }
+
+    const newProb = parseFloat(prompt("Probability: "));
+    const key = newRow1.toString() + "," + newCol1.toString();
+    const val = newRow2.toString() + "," + newCol2.toString() + "," + newProb.toString();
+    var newEdges = this.state.edges;
+
+    if (key in newEdges) {
+      var totalProb = 0;
+      for (let i = 0; i < newEdges[key].length; i++) {
+        var triple = newEdges[key][i].split(",");
+        totalProb += parseFloat(triple[2]);
+      }
+      if (totalProb + newProb > 1) {
+        alert("Total probability out of state " + this.grid[newRow1][newCol1] + " may not exceed 1");
+        return; // improve
+      } else {
+        newEdges[key].push(val);
+      }
+    } else {
+      if (newProb <= 1) newEdges[key] = [val];
+    }
+
+    console.log(newEdges);
+    this.setState({ edges: newEdges });
+  }
+
+  // gray out node's neighbors, row, and col
+  grayNodes = (row, col) => {
+    for (let r = 0; r < this.grid.length; r++) {
+      for (let c = 0; c < this.grid[row].length; c++) {
+        if ((r === row || c === col) && !(r === row && c === col)) {
+          this.grid[r][c] = null;
+        }
+      }
+    }
+
     for (var dr of [-1, 0, 1]) {
       for (var dc of [-1, 0, 1]) {
-        var newRow = row + dr
-        var newCol = col + dc
         if (!(dr === 0 && dc === 0)
-          && newRow >= 0 && newRow < this.numRows
-          && newCol >= 0 && newCol < this.numCols) {
-          this.model[newRow][newCol] = null;
+          && row + dr >= 0 && row + dr < this.numRows
+          && col + dc >= 0 && col + dc < this.numCols) {
+          this.grid[row + dr][col + dc] = null;
         }
       }
     }
   }
 
+  // return updated nodes matrix
   updateRender() {
-    for (let row = 0; row < this.model.length; row++) {
-      for (let col = 0; col < this.model[row].length; col++) {
-        const label = this.model[row][col];
+    for (let row = 0; row < this.grid.length; row++) {
+      for (let col = 0; col < this.grid[row].length; col++) {
+        const label = this.grid[row][col];
         if (label !== null && label.length > 0) {
-          this.grayNeighbors(row, col);
+          this.grayNodes(row, col);
         }
       }
     }
 
     let newRender = []
-
-    for (let row = 0; row < this.model.length; row++) {
+    for (let row = 0; row < this.grid.length; row++) {
       const currRow = [];
-      for (let col = 0; col < this.model[row].length; col++) {
-        const currNode = {row, col, label: this.model[row][col]};
+      for (let col = 0; col < this.grid[row].length; col++) {
+        const currNode = { row, col, label: this.grid[row][col] };
         currRow.push(currNode);
       }
       newRender.push(currRow);
     }
-
     return newRender
   }
 
   updateLabel = (row, col) => {
-    var newLabel = prompt("Please enter a new label: ");
-    this.model[row][col] = newLabel;
+    var newLabel = prompt("Please enter a new label: "); // improve
+    this.grid[row][col] = newLabel;
     this.setState({ nodes: this.updateRender() });
   }
 
   componentDidMount() {
-    this.generateModel();
+    this.generateGrid();
     this.setState({ nodes: this.updateRender() });
   }
 
   render() {
     const { nodes } = this.state;
 
+    // improve 
     return (
       <div className="grid">
-        {nodes.map((row, rowIdx) => {
-          return (<div key={rowIdx}>
-            {row.map((node) => {
-              const { row, col, label } = node;
-              return (
-                <Node key={(row, col)}
-                  row={row}
-                  col={col}
-                  label={label}
-                  model={this.model}
-                  updateLabel={this.updateLabel}>
-                </Node>
-              );
-            })}
-          </div>
-          );
-        })}
+        <div className="button" onClick={() => this.addEdge()}>
+          ADD EDGE
+        </div>
+        <div className="nodes">
+          {nodes.map((row, rowIdx) => {
+            return (<div key={rowIdx}>
+              {row.map((node) => {
+                const { row, col, label } = node;
+                return (
+                  <Node key={(row, col)}
+                    row={row}
+                    col={col}
+                    label={label}
+                    grid={this.grid}
+                    updateLabel={this.updateLabel}>
+                  </Node>
+                );
+              })}
+            </div>
+            );
+          })}
+        </div>
       </div>
     )
   }
